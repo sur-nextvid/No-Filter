03bcce0ec1d428c5e01cd4a422cbc826e45d8523
Yasin Shuman <email@yasin.io>
2018-08-13 02:32:43 -0700

    added vote to struct and upvote func and event #8

--- contracts/nofilter.sol
+++ contracts/nofilter.sol
@@ -12,30 +12,44 @@ contract NoFilter {
         address ownerId;
         string description;
         string[] tags;
+        uint vote;
     }
-  
+    
+    
+    
+    event Vote(
+      address voter,        
+      bytes32 indexed _item,
+      uint vote
+    );
+    
     event Alert(
-        address indexed owner,
+        address indexed ownerId,
         bytes32 indexed _item
     );
     
     mapping(bytes32 => Item) public details;
-    
+
+    function upVote(bytes32 _item) public payable returns (uint) {
+        details[_item].vote += 1;
+        emit Vote(msg.sender, _item, details[_item].vote);
+        return details[_item].vote;
+    }
+
     function upload(address _ownerId, string _description, string[] _tags, bytes32 _item ) public {
-        details[_item] = Item(_ownerId, _description, _tags);
+        uint beginningVote = 1;
+        details[_item] = Item(_ownerId, _description, _tags, beginningVote );
         emit Alert(msg.sender, _item);
     }
     
     function getDetails(bytes32 _item) public view returns (
         address,
         string,
-        string[] 
-    )   {
-        return (
-            details[_item].ownerId,
-            details[_item].description,
-            details[_item].tags
-        );
+        string[],
+        uint
+    ) {
+       return (details[_item].ownerId,
+        details[_item].description,
+        details[_item].tags,
+        details[_item].vote);
     }
-
-}
